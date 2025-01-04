7b3ba0c2d8fb96307dbb0fa78a4d2b61c5503362
Sriganesh <git@sriz.net>
2025-01-03 22:44:26 -0500

    Refactor MobileBottomBar and MobileMoreMenu components
    - Reintroduced the 'More Menu' button in the MobileBottomBar component, ensuring consistent functionality and layout.
    - Adjusted the positioning of the MobileMoreMenu to align with the right side of the screen for improved accessibility.
    - Enhanced user experience by maintaining the toggle functionality for the 'More Menu' while ensuring it integrates seamlessly with the existing mobile navigation structure.

--- src/components/MobileBottomBar.tsx
+++ src/components/MobileBottomBar.tsx
@@ -66,22 +66,6 @@ export function MobileBottomBar({ theme, onShowSearch, onShowSettings }: MobileB
           </svg>
         </button>
 
-        {/* More Menu Button */}
-        <button
-          onClick={() => setShowMoreMenu(!showMoreMenu)}
-          className="p-4 flex items-center justify-center relative more-menu-container"
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-            <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
-          </svg>
-
-          <MobileMoreMenu 
-            theme={theme}
-            showMenu={showMoreMenu}
-            onClose={() => setShowMoreMenu(false)}
-          />
-        </button>
-
         {/* Search Button */}
         <button
           onClick={onShowSearch}
@@ -100,6 +84,22 @@ export function MobileBottomBar({ theme, onShowSearch, onShowSettings }: MobileB
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
             <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
           </svg>
+        </button>
+
+        {/* More Menu Button */}
+        <button
+          onClick={() => setShowMoreMenu(!showMoreMenu)}
+          className="p-4 flex items-center justify-center relative more-menu-container"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
+          </svg>
+
+          <MobileMoreMenu 
+            theme={theme}
+            showMenu={showMoreMenu}
+            onClose={() => setShowMoreMenu(false)}
+          />
         </button>
       </div>
     </div>
