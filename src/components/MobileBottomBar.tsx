61c57a5eda5fd3d0672da2d3905f38485c7b514a
Sriganesh <git@sriz.net>
2025-02-20 20:50:17 -0500

    Add unread count badge to Profile button in MobileBottomBar

--- src/components/MobileBottomBar.tsx
+++ src/components/MobileBottomBar.tsx
@@ -132,10 +132,21 @@ export function MobileBottomBar({ theme, onShowSearch, onCloseSearch, onShowSett
             }}
             className="flex flex-col items-center"
           >
-            <div className={`${iconClass} opacity-50 hover:opacity-75`}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-              </svg>
+            <div className="relative">
+              <div className={`${iconClass} opacity-50 hover:opacity-75`}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>
+              </div>
+              {unreadCount > 0 && (
+                <div className={`absolute -top-1 -right-1 min-w-[1.25rem] h-5 rounded-full flex items-center justify-center text-xs ${
+                  theme === 'green'
+                    ? 'bg-green-500 text-black'
+                    : 'bg-[#ff6600] text-white'
+                }`}>
+                  {unreadCount}
+                </div>
+              )}
             </div>
             <span className={labelClass}>Profile</span>
           </button>
@@ -179,15 +190,6 @@ export function MobileBottomBar({ theme, onShowSearch, onCloseSearch, onShowSett
                     <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
                 </div>
-                {unreadCount > 0 && (
-                  <div className={`absolute -top-1 -right-1 min-w-[1.25rem] h-5 rounded-full flex items-center justify-center text-xs ${
-                    theme === 'green'
-                      ? 'bg-green-500 text-black'
-                      : 'bg-[#ff6600] text-white'
-                  }`}>
-                    {unreadCount}
-                  </div>
-                )}
               </div>
               <span className={labelClass}>More</span>
             </button>
